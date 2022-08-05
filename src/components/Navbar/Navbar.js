bb39fe610aa250337a22692b503e7edaa42f73cf
Koranteng Anane <squidiw.squidiw@gmail.com>
2022-08-05 10:23:35 -0400

    Navbar little update
    Route currently in index.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import { Link, Outlet } from "react-router-dom";
 const Navbar = () => {
    return (
       <div>
-         <Link to="/">Home</Link>
+         <Link to="/">Home link</Link>
          <>
             <Outlet />
          </>
